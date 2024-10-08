451f72c42bb5fa08ef8d945cc2931fcc4ca96961
diedrizon <diedrinzonfargas@gmail.com>
2024-10-08 17:39:47 -0600

    :white_check_mark: Graficos generados de las dos practicas

---
+++ firebaseconfig.js
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore"; // Importar Firestore
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC26erOKmGGgUcdiQmQyDT9aQwOmAdkN-s",
+  authDomain: "pruebas-8d4a1.firebaseapp.com",
+  projectId: "pruebas-8d4a1",
+  storageBucket: "pruebas-8d4a1.appspot.com",
+  messagingSenderId: "310106250145",
+  appId: "1:310106250145:web:ae6568acc7c629772389e0",
+  measurementId: "G-V06BMSDZ8T"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const db = getFirestore(app); // Inicializar Firestore
+
+export default db; // Exportar la instancia de Firestore
